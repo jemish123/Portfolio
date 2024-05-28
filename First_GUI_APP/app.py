11faa61df83afb0868e02fc1600eb2225fc5325e
jemish123 <jemishgm103@gmail.com>
2024-05-28 18:10:53 +0530

    Second GUI Project Commit

--- First_GUI_APP/app.py
+++ First_GUI_APP/app.py
@@ -1,42 +1,28 @@
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
-from kivy.graphics import Rectangle
-from kivy.uix.widget import Widget
-from kivy.core.window import Window
-
-ROWS = COLS = 3
+from kivy.uix.textinput import TextInput
 
 
-# class GridApp(App):
-#     def build(self, ):
-#         root = GridLayout(rows=ROWS, cols=COLS)
-#         for i in range(ROWS):
-#             for j in range(COLS):
-#                 root.add_widget(Button(text=f"({i}, {j})"))
-#
-#         return root
+class LoginScreen(GridLayout):
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 2
+
+        # creating widgets
+        self.add_widget(Label(text="Username"))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+
+        self.add_widget(Label(text="Password"))
+        self.password = TextInput(password=True, multiline=False)
+        self.add_widget(self.password)
 
 
-# class MainApp(App):
-#     def build(self):
-#         return GridApp()
-
-class CanvasApp(App):
+class FirstApp(App):
     def build(self):
-        root = Widget()
-        width = Window.size[0] * 0.95
-        height = Window.size[1] * 0.95
-        pos_x_1 = 10
-        pos_y_1 = 10
-        pos_x_2 = Window.size[0] - 10
-        pos_y_2 = Window.size[1] - 10
-
-        with root.canvas:
-            Rectangle(size=[width, height], pos=[pos_x_1, pos_y_1])
-
-        return root
+        return LoginScreen()
 
 
-CanvasApp().run()
+if __name__ == "__main__":
+    FirstApp().run()
